1825b8b70f08ebf3afaf3031825d61355ead8a2e
andychenggg <2768054177@qq.com>
2023-03-08 18:26:22 +0800

    123

---
+++ src/main/java/lab4/StreamProcessingOrder.java
@@ -0,0 +1,124 @@
+package lab4;
+
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+public class StreamProcessingOrder {
+    public static void main(String[] args) {
+
+        Stream.of("CS", "209", "A").filter(s -> {
+            System.out.println("filter: " + s);
+            return true;
+        });
+        //nothing is printed to the console.
+
+        Stream.of("CS", "209", "A").filter(s -> {
+            System.out.println("filter: " + s);
+            return true;
+        }).forEach(s -> System.out.println("forEach: " + s));
+        //output:
+        //filter: CS
+        //forEach: CS
+        //filter: 209
+        //forEach: 209
+        //filter: A
+        //forEach: A
+
+        Stream.of("CS", "209", "A").map(s -> {
+            System.out.println("map: " + s);
+            return s.toLowerCase();
+        })
+                .anyMatch(s -> {
+                    System.out.println("anyMatch: " + s);
+                    return s.startsWith("c");
+                });
+        //output:
+        //map: CS
+        //anyMatch: cs
+
+        Stream.of("209", "CS", "303", "A", "B")
+                .sorted((s1, s2) -> {
+                    System.out.printf("sort: %s; %s\n", s1, s2);
+                    return s1.compareTo(s2);
+                })
+                .filter(s -> {
+                    System.out.println("filter: " + s);
+//                    return s.startsWith("C");
+                    return true;
+                })
+                .map(s -> {
+                    System.out.println("map: " + s);
+                    return s.toLowerCase();
+                })
+                .limit(10)
+                .forEach(s -> System.out.println("forEach: " + s));
+        //output:
+        //sort: CS; 209
+        //sort: 303; CS
+        //sort: 303; CS
+        //sort: 303; 209
+        //sort: A; 303
+        //sort: A; CS
+        //sort: B; A
+        //sort: B; CS
+        //filter: 209
+        //filter: 303
+        //filter: A
+        //filter: B
+        //filter: CS
+        //map: CS
+        //forEach: cs
+
+        Stream.of("209", "CS", "303", "A", "B")
+                .filter(s -> {
+                    System.out.println("filter: " + s);
+                    return s.startsWith("C");
+                })
+                .sorted((s1, s2) -> {
+                    System.out.printf("sort: %s; %s\n", s1, s2);
+                    return s1.compareTo(s2);
+                })
+                .map(s -> {
+                    System.out.println("map: " + s);
+                    return s.toLowerCase();
+                })
+                .forEach(s -> System.out.println("forEach: " + s));
+        //output:
+        //filter: 209
+        //filter: CS
+        //filter: 303
+        //filter: A
+        //filter: B
+        //map: CS
+        //forEach: cs
+
+        System.out.println("======Reuse Streams======");
+
+        Stream<String> stream =
+                Stream.of("CS", "209", "A")
+                        .filter(s -> s.startsWith("C"));
+
+        stream.anyMatch(s -> true);    // ok
+        stream.forEach(System.out::println);   // exception
+
+        Supplier<Stream<String>> streamSupplier =
+                () -> Stream.of("CS", "209", "A")
+                        .filter(s -> s.startsWith("C"));
+
+        streamSupplier.get().anyMatch(s -> true);   // ok
+        streamSupplier.get().forEach(System.out::println);  // ok
+
+
+//        Supplier<Stream<String>> supplier = () -> Stream.of("CS", "209", "A");
+//        supplier.get().forEach(System.out::println);
+//        supplier.get().forEach(System.out::println);
+
+
+        Stream<String> ss = Stream.of("CS", "209", "A");
+        Supplier<Stream<String>> s = () -> ss; //same stream ss, cannot be reused
+        //Supplier<Stream<String>> s = ()->Stream.of("CS", "209", "A");
+        s.get().forEach(System.out::println);
+        s.get().forEach(System.out::println);
+
+    }
+}
